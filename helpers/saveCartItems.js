c156832909d00a07856be0f6ec041d3dd1482bb3
carlatakagi <carlatakagi@gmail.com>
2022-01-19 16:44:52 -0400

    correção helpers

--- helpers/saveCartItems.js
+++ helpers/saveCartItems.js
@@ -1,7 +1,7 @@
 // deve salvar os itens do carrinho de compras no localStorage, em uma chave denominada 'cartItems'
 // recebe chave e valor https://developer.mozilla.org/pt-BR/docs/Web/API/Storage/setItem
-const saveCartItems = (carItems) => {
-  localStorage.setItem('cartItems', carItems);
+const saveCartItems = (cartItems) => {
+  localStorage.setItem('cartItems', cartItems);
 };
 
 if (typeof module !== 'undefined') {
