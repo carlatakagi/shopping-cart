1b9cd610a8e832c3b8a7c6b793ca9657356e8732
carlatakagi <carlatakagi@gmail.com>
2022-01-19 16:40:26 -0400

    adicionando testes

--- tests/fetchProducts.test.js
+++ tests/fetchProducts.test.js
@@ -3,9 +3,6 @@ const { fetchProducts } = require('../helpers/fetchProducts');
 const computadorSearch = require('../mocks/search');
 
 describe('1 - Teste a função fecthProducts', () => {
-  // implemente seus testes aqui
-  // fail('Teste vazio');
-
   it('Teste se fetchProducts é uma função', () => {
     expect(typeof fetchProducts).toBe('function');
   });
--- tests/getSavedCartItems.test.js
+++ tests/getSavedCartItems.test.js
@@ -3,7 +3,14 @@ const getSavedCartItems = require('../helpers/getSavedCartItems');
 
 localStorageSimulator('getItem');
 
-describe('4 - Teste a função getSavedCartItems', () => {
-  // implemente seus testes aqui
-  fail('Teste vazio');
+describe('4 - Teste a função getSavedCartItems', () => {  
+  it('Teste se, ao executar getSavedCartItems, o método localStorage.getItem é chamado;', () => {
+    getSavedCartItems();
+    expect(localStorage.getItem).toHaveBeenCalled();
+  });
+
+  it('Teste se, ao executar getSavedCartItems, o método localStorage.getItem é chamado com o cartItems como parâmetro.', () => {
+    getSavedCartItems('cartItems');
+    expect(localStorage.getItem).toHaveBeenCalledWith('cartItems');
+  })
 });
